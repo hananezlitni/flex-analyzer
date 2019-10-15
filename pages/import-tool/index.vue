e5f589384d357130a6123b52533f84e87b868e34
Hanane Zlitni <hananzlitni@hotmail.com>
2019-10-14 23:25:28 -0400

    Generate figure automatically without button click

--- pages/import-tool/index.vue
+++ pages/import-tool/index.vue
@@ -7,7 +7,7 @@
                     <p class="import__note"><i>Please use the form below to import a structure as vectors. Please read <a href="/">the documentation</a> for information about the format of the content.</i></p>
 
                     <form class="import__vectors-form" @submit="$event.preventDefault()">
-                        <input type="file" id="file-upload" class="import__vectors-form__input" @change="getCsvVectors($event)" />
+                        <input type="file" id="file-upload" class="import__vectors-form__input" @change="getVectorsFromCsv($event)" />
                         <label for="file-upload" class="import__vectors-form__label">
                             <svg xmlns="http://www.w3.org/2000/svg" width="25" height="15" viewBox="0 0 512 512"><path fill="#dddddd" d="M296 384h-80c-13.3 0-24-10.7-24-24V192h-87.7c-17.8 0-26.7-21.5-14.1-34.1L242.3 5.7c7.5-7.5 19.8-7.5 27.3 0l152.2 152.2c12.6 12.6 3.7 34.1-14.1 34.1H320v168c0 13.3-10.7 24-24 24zm216-8v112c0 13.3-10.7 24-24 24H24c-13.3 0-24-10.7-24-24V376c0-13.3 10.7-24 24-24h136v8c0 30.9 25.1 56 56 56h80c30.9 0 56-25.1 56-56v-8h136c13.3 0 24 10.7 24 24zm-124 88c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20zm64 0c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20z"/></svg>
                             Choose a file
@@ -15,8 +15,8 @@
 
                         <p id="errorMessage"></p>
                         
-                        <textarea placeholder="Vectors..." id="textArea" class="import__vectors-form__textarea" @keyup="getCsvData($event)" />
-                        <input class="button button--action primary import__button" type="submit" value="Generate Figure" @click="generateFigure">
+                        <textarea placeholder="Vectors..." id="textArea" class="import__vectors-form__textarea" @keyup="parseCsvData($event)" />
+                        <input class="button button--action primary import__button" type="submit" value="Get Results">
                     </form>
                 </div>
                 <div id="figure"></div> 
@@ -27,7 +27,7 @@
                     <p class="import__note"><i>Please use the form below to import a structure as configurations. Please read <a href="/">the documentation</a> for information about the format of the content.</i></p>
 
                     <form class="import__configurations-form" @submit="$event.preventDefault()">
-                        <input type="file" id="file-upload" class="import__configurations-form__input" @change="getCsvConfigurations($event)" />
+                        <input type="file" id="file-upload" class="import__configurations-form__input" @change="getConfigurationsFromCsv($event)" />
                         <label for="file-upload" class="import__vectors-form__label">
                             <svg xmlns="http://www.w3.org/2000/svg" width="25" height="15" viewBox="0 0 512 512"><path fill="#dddddd" d="M296 384h-80c-13.3 0-24-10.7-24-24V192h-87.7c-17.8 0-26.7-21.5-14.1-34.1L242.3 5.7c7.5-7.5 19.8-7.5 27.3 0l152.2 152.2c12.6 12.6 3.7 34.1-14.1 34.1H320v168c0 13.3-10.7 24-24 24zm216-8v112c0 13.3-10.7 24-24 24H24c-13.3 0-24-10.7-24-24V376c0-13.3 10.7-24 24-24h136v8c0 30.9 25.1 56 56 56h80c30.9 0 56-25.1 56-56v-8h136c13.3 0 24 10.7 24 24zm-124 88c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20zm64 0c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20z"/></svg>
                             Choose a file
@@ -36,7 +36,7 @@
                         <p id="errorMessage"></p>
 
                         <textarea placeholder="Configurations..." class="import__configurations-form__textarea" />
-                        <input class="button button--action primary import__button" type="submit" value="Generate Figure" @click="loadConfigurations">
+                        <input class="button button--action primary import__button" type="submit" value="Get Results">
                     </form>
                 </div>
             </tab>
@@ -91,7 +91,6 @@
                             continue
                         }
                     }
-
                     //Check fvector dimensions
                     for (var j = 0; j < this.fVector.length; j++) {
                         if (this.fVector.length != this.numOfServers || this.fVector[j].length != this.numOfTasks) {
@@ -114,10 +113,7 @@
                     }
                 }
             },
-            loadConfigurations() {
-                console.log("loadConfigurations clicked!")
-            },
-            getCsvVectors(e) {
+            getVectorsFromCsv(e) {
                 let input = document.getElementById('file-upload');
                 let textarea = document.getElementById('textArea');
                 var self = this
@@ -147,10 +143,11 @@
                         for (i = fVectorStart; i < values.length; i++) {
                             self.fVector[count++] = values[i].split(',')
                         }
+                        self.generateFigure()
                     };
                 }
             },
-            getCsvData(e) {
+            parseCsvData(e) {
                 this.fVector = []
                 let textarea = e.target.value;
                 let values = textarea.split("\n")
@@ -163,8 +160,9 @@
                 for (var j = fVectorStart; j < values.length; j++) {
                     this.fVector[count++] = values[j].split(',')
                 }
+                this.generateFigure()
             },
-            getCsvConfigurations(e) {
+            getConfigurationsFromCsv(e) {
                 
             },
             errorMessage(message) {
