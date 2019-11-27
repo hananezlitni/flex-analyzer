87711f1675b3aa641246273991be0755558e0be2
Hanane Zlitni <hananzlitni@hotmail.com>
2019-11-26 19:47:42 -0500

    Display name of uploaded file + accept only csv file uploads

--- pages/index.vue
+++ pages/index.vue
@@ -8,7 +8,8 @@
 
                     <form class="import__vectors-form" @submit="$event.preventDefault()">
                         <div class="buttons-div">
-                          <input type="file" id="file-upload" class="import__vectors-form__input" @change="displayCsvDataInTextArea"/>
+                          <input type="text" id="file-name" class="import__vectors-form__file-name" readonly />
+                          <input type="file" id="file-upload" class="import__vectors-form__input" accept=".csv" @change="displayCsvDataInTextArea"/>
                           <label for="file-upload" class="import__vectors-form__label">
                               <svg xmlns="http://www.w3.org/2000/svg" width="25" height="15" viewBox="0 0 512 512"><path fill="#dddddd" d="M296 384h-80c-13.3 0-24-10.7-24-24V192h-87.7c-17.8 0-26.7-21.5-14.1-34.1L242.3 5.7c7.5-7.5 19.8-7.5 27.3 0l152.2 152.2c12.6 12.6 3.7 34.1-14.1 34.1H320v168c0 13.3-10.7 24-24 24zm216-8v112c0 13.3-10.7 24-24 24H24c-13.3 0-24-10.7-24-24V376c0-13.3 10.7-24 24-24h136v8c0 30.9 25.1 56 56 56h80c30.9 0 56-25.1 56-56v-8h136c13.3 0 24 10.7 24 24zm-124 88c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20zm64 0c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20z"/></svg>
                               Choose a file
@@ -175,6 +176,8 @@
               if (input.files && input.files[0]) { 
                   var myFile = input.files[0];
                   var reader = new FileReader();
+
+                  document.getElementById('file-name').value = myFile.name
                   reader.readAsBinaryString(myFile)
 
                   reader.onload = function (e) {
@@ -233,7 +236,6 @@
               }*/
             },
             storeInputs() { //make sure latest inputs are stored when click generate figure and submit structure buttons.. validation needs to happen for both as well in case f matrix was edited just before submission.
-              let input = document.getElementById('file-upload');
               let textarea = document.getElementById('textArea');
 
               //Reset anyway in case there's previous content
@@ -883,6 +885,24 @@
     }
     .import__vectors-form__input + label *, .import__configurations-form__input + label * {
         pointer-events: none;
+    }
+    .import__vectors-form__file-name {
+      width: 300px;
+      height: 43px;
+      padding: 10px;
+      border-top: none;
+      border-left: none;
+      border-right: none;
+      border-bottom: 2px solid $accent-color;
+      background-color: $background-color--main;
+      color: $font-color;
+      font-size: 1em;
+      font-family: $roboto;
+      line-height: 1.5;
+      margin-right: 1.5em;
+      &:focus {
+        outline: none;
+      }
     }
     .buttons-div {
       display: flex;
