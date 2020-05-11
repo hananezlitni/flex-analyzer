59c48b271af3b8963dbebdf5ff0e91726d06456b
Hanane Zlitni <hananzlitni@hotmail.com>
2020-05-11 02:55:17 -0400

    Potential deployment fix

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ cors = CORS(application, resources={r"/*": {"origins": "*"}})
 
 @application.route("/")
 def index():
-    return app.render_template('index.html')
+    return render_template('index.html')
 
 @application.route('/', methods=["POST"])
 def app():
