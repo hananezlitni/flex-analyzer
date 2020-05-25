9882cc73b607c85b742c51aa196e7d8277ef796a
Hanane Zlitni <hananzlitni@hotmail.com>
2020-05-24 23:47:29 -0400

    Potential deployment fix - Serve dist folder and push it to repo

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_cors import CORS
 from scripts import solver 
 from random import *
 
-application = Flask(__name__, template_folder='./templates')
+application = Flask(__name__)
 cors = CORS(application, resources={r"/*": {"origins": "*"}})
 
 #print(sys.path)
@@ -13,6 +13,11 @@ cors = CORS(application, resources={r"/*": {"origins": "*"}})
 @application.route("/")
 def index():
     return send_from_directory('./dist/', 'index.html') #return render_template('index.html')
+
+@application.route('/static/<path:filename>')
+def serve_static(filename):
+    root_dir = os.path.dirname(os.getcwd())
+    return send_from_directory(os.path.join(root_dir, 'static', 'js'),   filename)
 
 @application.route('/', methods=["POST"])
 def app():
