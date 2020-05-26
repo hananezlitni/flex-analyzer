8ebe7c9939775e37a18e3f3452ebad5eeed9f20f
Hanane Zlitni <hananzlitni@hotmail.com>
2020-05-25 20:46:22 -0400

    Potential deployment fix

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 #!flask/bin/python3.8
 import os
-from flask import Flask, request, jsonify, render_template, send_from_directory
+from flask import Flask, request, jsonify, render_template, send_from_directory, send_static_file
 from flask_cors import CORS
 from scripts import solver 
 from random import *
 
-application = Flask(__name__)
+application = Flask(__name__, static_folder="static")
 cors = CORS(application, resources={r"/*": {"origins": "*"}})
 
 #print(sys.path)
@@ -14,10 +14,18 @@ cors = CORS(application, resources={r"/*": {"origins": "*"}})
 #def index():
 #    return render_template('index.html')
 
-@application.route('/', defaults={'path': ''})
-@application.route('/<path:path>')           
-def index(path):
-    return render_template('index.html')
+#@application.route('/', defaults={'path': ''})
+#@application.route('/<path:path>')           
+#def index(path):
+#    return render_template('index.html')
+
+@application.route("/")
+def index():
+    return application.send_static_file('../client/index.html')
+
+@application.route('/<path:path>')
+def serve_page(path):
+    return send_from_directory('static', path)
 
 @application.route('/', methods=["POST"])
 def app():
