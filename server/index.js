d9c6241aa4743219b510a020a9297279769286f3
Hanane Zlitni <hananzlitni@hotmail.com>
2020-09-18 18:20:04 -0400

    Render nuxt in the server

--- server/index.js
+++ server/index.js
@@ -1,12 +1,33 @@
+const { loadNuxt, build } = require('nuxt')
 const express = require("express");
 const bodyParser = require("body-parser");
 var cors = require('cors')
 const router = express.Router();
 const app = express();
+const isDev = process.env.NODE_ENV !== 'production'
+const port = process.env.PORT || 3000
 
 app.use(cors())
 app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: true }));
+
+async function start() {
+  // We get Nuxt instance
+  const nuxt = await loadNuxt(isDev ? 'dev' : 'start')
+
+  // Render every route with Nuxt.js
+  app.use(nuxt.render)
+
+  // Build only in dev mode with hot-reloading
+  if (isDev) {
+    build(nuxt)
+  }
+  // Listen the server
+  app.listen(port, '0.0.0.0')
+  console.log('Server listening on `localhost:' + port + '`.')
+}
+
+start()
 
 router.post('/',(req, res) => {
   var aMatrix = req.body.aMatrix;
